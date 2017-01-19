d9e8598fdf11ac38d47df0cda30412c6c67a27a5
Fedor Hajdu <fedorhajdu@gmail.com>
2017-01-19 09:20:47 +0400

    Add password to instance

--- build_redis.sh
+++ build_redis.sh
@@ -11,6 +11,9 @@ pushd $REDIS_BUILD_DIR
 make
 sudo make install
 cp src/redis-trib.rb /usr/local/bin/redis-trib.rb
+
+sed -i "s/^# requirepass foobared/requirepass mypassword/g" ./redis.conf
+
 cp ./redis.conf /etc/redis.conf
 
 popd
